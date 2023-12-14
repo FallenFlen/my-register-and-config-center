50f7b937fe23cf916b35af737278b4a51afbd543
Fallenz <beijuspsn@163.com>
2023-12-14 15:59:43 +0800

    update sql

--- config-service/src/main/resources/db/create_config.sql
+++ config-service/src/main/resources/db/create_config.sql
@@ -5,6 +5,7 @@ CREATE TABLE config
     `type`                       VARCHAR(100) NOT NULL COMMENT '配置类型',
     `status`                     VARCHAR(100) NOT NULL COMMENT '状态',
     `belonging_application_name` VARCHAR(200) NOT NULL COMMENT '归属应用名称',
+    `file_name` VARCHAR(200) NOT NULL COMMENT '配置文件名称',
     description                  text NULL COMMENT '描述',
     md5                          VARCHAR(32)  NOT NULL COMMENT 'md5',
     create_by                    VARCHAR(100) NULL COMMENT '创建人',
